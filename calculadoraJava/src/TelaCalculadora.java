76813240092c3c59c02687e71fb1cba7b4622839
Petrosski <lucas.oening07@gmail.com>
2023-03-20 12:27:30 -0300

    att

--- calculadoraJava/src/TelaCalculadora.java
+++ calculadoraJava/src/TelaCalculadora.java
@@ -2,4 +2,23 @@ import javax.swing.*;
 
 public class TelaCalculadora {
     private JTextField textField1;
+    private JPanel PNLTelaCalculadora;
+    private JButton a00Button;
+    private JButton a0Button;
+    private JButton button4;
+    private JButton a3Button;
+    private JButton a2Button;
+    private JButton a1Button;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton a7Button;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton button18;
+    private JButton button1;
+    private JButton btnRaiz; //√
 }
